c4bb8d71093c1d08ef3ec10a9a2abeede2c43963
Rafał Barnaś <125920366+rapheeck@users.noreply.github.com>
2023-04-14 18:20:18 +0200

    import React from react

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
-import { useState } from "react";
+import React, { useState } from "react";
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
